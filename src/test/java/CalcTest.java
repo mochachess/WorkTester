2d439cf741a2a0c9f24bba61fef0c6b311d6664c
mochachess <mentos_vist@mail.ru>
2019-09-11 13:06:29 +0300

    заполнение форм, вход\выход с гита

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -3,11 +3,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.pagefactory.ByAll;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import sun.awt.image.PixelConverter;
+
+import java.awt.*;
 
 public class CalcTest {
 
@@ -32,55 +36,24 @@ public class CalcTest {
 
     @Test
     public void test() throws InterruptedException {
-//        driver.get("https://www.performance-lab.ru/");
-//        Thread.sleep(500);
-//        driver.findElement(By.xpath("//*[@id='menu-item-2613']/a")).click();
-//        Thread.sleep(500);
-//        driver.findElement(By.xpath("//*[@id='page-wrapper']/div[2]/div[2]/div/div[2]/div[1]/article/h2/a")).click();
-//        Thread.sleep(500);
 
-//        WebElement webElement = driver.findElement(By.xpath(".//*[text()='Рис. 2. Страница отчета со списком дефектов']"));
-        Assert.assertTrue(true);
 
-        driver.get("https://habr.com/ru/");
-        Thread.sleep(1500);
-        driver.findElement(By.xpath("//button[@id='search-form-btn']")).click();
+        driver.manage().window().maximize();
+        driver.get("https://github.com/");
         Thread.sleep(500);
-        driver.findElement(By.xpath(".//*[text()='Неделя']")).click();
-        Thread.sleep(1500);
-        driver.get("https://habr.com/ru/");
+        driver.findElement(By.xpath("//a[@href='/login']")).click();
         Thread.sleep(500);
-        driver.findElement(By.xpath("//a[@href='https://tmtm.ru/']")).click();
-        Thread.sleep(1500);
-        driver.get("https://habr.com/ru/");
-        Thread.sleep(1500);
-        driver.findElement(By.xpath("//a[@href='https://telegram.me/habr_com']")).click();
-        Thread.sleep(1500);
+        driver.findElement(By.id("login_field")).sendKeys("mentos_vist@mail.ru");
+        driver.findElement(By.id("password")).sendKeys("!");
+        driver.findElement(By.name("commit")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//summary[@aria-label='View profile and more']")).click();
+        Thread.sleep(500);
 
-        driver.get("https://mail.ru/");
-        Thread.sleep(1500);
-        driver.findElement(By.xpath("//a [@id='mailbox:mailHeaderLink']")).click();
-        Thread.sleep(1500);
-        driver.get("https://mail.ru/");
-        Thread.sleep(2500);
-        driver.findElement(By.xpath("//a[@title='Облако']")).click();
-        Thread.sleep(1500);
-
-        driver.get("https://mail.ru/");
-        Thread.sleep(2500);
-        driver.findElement(By.xpath("//form[@action='//go.mail.ru/search']")).click();
-        Thread.sleep(1500);
-
-//        driver.get("https://google.ru/");
-//        Thread.sleep(1500);
-//        driver.findElement(By.xpath("//input[@name='btnI']")).click();
-//        Thread.sleep(1500);
-
-
-//        WebElement webElement = driver.findElement(By.xpath(".//*[text()='Рис. 2. Страница отчета со списком дефектов']"));
-//        Assert.assertTrue(webElement.isDisplayed());
-
-
+        String wb = driver.findElement(By.xpath("//strong[@class='css-truncate-target']")).getText();
+        String name = "mochaches";
+        Assert.assertEquals(wb, name);
+        driver.findElement(By.xpath("//button[@class='dropdown-item dropdown-signout']")).click();
     }
 
 
